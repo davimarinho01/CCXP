f435d4f45b69a3f4fd600d91d8d3193393874705
Davi Marinho <davimarinho01@outlook.com.br>
2023-12-27 09:33:38 -0300

    Second Commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hora = document.getElementById('hora')
 const minuto = document.getElementById('minuto')
 const segundo = document.getElementById('segundo')
 
-const lancamento = "16 dec 2023"
+const lancamento = "31 dec 2023"
 
 function countDown(){
     const dataLanc = new Date(lancamento)
